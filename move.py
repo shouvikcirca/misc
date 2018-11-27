3bb2a5c9307a0a0168b42c6092c84626e259d631
Rhett <goldenfibo1123@gmail.com>
2018-11-27 23:01:27 +0530

    removed anim.py and added move.py

---
+++ move.py
@@ -0,0 +1,38 @@
+import os, time
+
+
+str1=" _\n"
+str2="|_|"
+
+
+ltr1=" _\n"
+ltr2="|_|"
+
+
+for i in range(20):
+	stri=str1+str2
+	ltr=ltr1+ltr2
+	print (stri)
+	print (ltr)
+	time.sleep(0.1)
+	os.system('clear')
+	str1="\n"+str1
+	# str2="\n"+str2
+	ltr1=" "+ltr1
+	ltr2=" "+ltr2
+
+
+for i in range(20):
+	stri=str1+str2
+	ltr=ltr1+ltr2
+	print (stri)
+	print (ltr)
+	time.sleep(0.1)
+	os.system('clear')
+	str1=str1[1:]
+	# str2="\n"+str2
+	ltr1=ltr1[1:]
+	ltr2=ltr2[1:]
+
+
+time.sleep(0.3)
