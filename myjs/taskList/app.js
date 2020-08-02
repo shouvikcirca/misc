9cab79355bc6d302d6f0b89861e1ca39561b58a7
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-08-03 01:02:29 +0530

    implemented local storage code in myjs/taskList

--- myjs/taskList/app.js
+++ myjs/taskList/app.js
@@ -8,12 +8,55 @@ loadEventListeners();
 
 function loadEventListeners()
 {
+     // DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks);
+
+     // Add Task event
     form.addEventListener('submit', addTask);
+    
+    // Remove Task event
     taskList.addEventListener('click', removeTask);
+    
+    // Clear Task event
     clearbtn.addEventListener('click', clearTasks);
+    
+    // Filter Tasks event
     filter.addEventListener('keyup', filterTasks);
 }
 
+// Get tasks from local storage
+function getTasks(e)
+{
+    let tasks;
+    if(localStorage.getItem('tasks') === null)
+    {
+        tasks = []
+    }
+    else
+    {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task)
+    {
+        const li = document.createElement('li');
+        li.className = 'collectionItem';
+
+        li.appendChild(document.createTextNode(task));
+        const link = document.createElement('a');
+
+        // If you want something to the right of a list item in materialize, it has to have 
+        // secondary-content class
+        link.className = 'delete-item secondary-content';
+
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+
+        li.appendChild(link);
+        taskList.appendChild(li);
+
+    });
+    
+}
 
 
 
@@ -26,29 +69,54 @@ function addTask(e)
 
     else
     {
-    // In Materialize if you want the ul's to look good your ul should have a class 'collection' 
-    // and the li's in it should have a class 'collectionItem' 
-    const li = document.createElement('li');
-    li.className = 'collectionItem';
+        // In Materialize if you want the ul's to look good your ul should have a class 'collection' 
+        // and the li's in it should have a class 'collectionItem' 
+        const li = document.createElement('li');
+        li.className = 'collectionItem';
 
-    li.appendChild(document.createTextNode(taskInput.value));
-    const link = document.createElement('a');
+        li.appendChild(document.createTextNode(taskInput.value));
+        const link = document.createElement('a');
 
-    // If you want something to the right of a list item in materialize, it has to have 
-    // secondary-content class
-    link.className = 'delete-item secondary-content';
+        // If you want something to the right of a list item in materialize, it has to have 
+        // secondary-content class
+        link.className = 'delete-item secondary-content';
 
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
 
-    li.appendChild(link);
-    taskList.appendChild(li);
+        li.appendChild(link);
+        taskList.appendChild(li);
 
-    taskInput.value='';
+        
+        storeTaskInLocalStorage(taskInput.value);
+        
+        taskInput.value='';
+
     }
-
-
     e.preventDefault();
 }
+
+
+
+function storeTaskInLocalStorage(task)
+{
+    let tasks;
+    if(localStorage.getItem('tasks') === null)
+    {
+        tasks = [];
+    }
+    else
+    {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        
+    }
+
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
+}
+
+
 
 
 function removeTask(e)
@@ -56,9 +124,44 @@ function removeTask(e)
     if(e.target.parentElement.classList.contains('delete-item'))
     {
         if(confirm('Are you sure about deleting'+' '+e.target.parentElement.parentElement.textContent))
+        {
             e.target.parentElement.parentElement.remove();
+
+            //remove from local storage
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+
+        }
+            
     }
 }
+
+
+
+function removeTaskFromLocalStorage(taskItem)
+{
+    let tasks;
+    if(localStorage.getItem('tasks') === null)
+    {
+        tasks = [];
+    }
+    else
+    {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index)
+    {
+        if(taskItem.textContent === task)
+        {
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
+}
+
 
 
 function clearTasks(e)
@@ -70,7 +173,22 @@ function clearTasks(e)
     while(taskList.firstChild)
         taskList.removeChild(taskList.firstChild);
 
+
+    // Clear tasks from local storage
+    clearTasksFromLocalStorage();
+
 }
+
+
+function clearTasksFromLocalStorage()
+{
+    localStorage.clear()
+}
+
+
+
+
+
 
 function filterTasks(e)
 {
