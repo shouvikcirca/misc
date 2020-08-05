095d90e6d085a8534a97f72e949c856be2fdd353
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-08-05 15:37:31 +0530

    quicksort_visualizer.py

---
+++ quicksort_visualizer.py
@@ -0,0 +1,79 @@
+import os
+import time
+import random
+
+
+def swap(array, a, b):
+    temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
+
+def partition(array, p, q):
+    pivot = q
+    i = p-1
+
+    for j in range(p,pivot):
+        if array[j]>array[pivot]:
+            i+=1
+            swap(array, i, j)
+
+    swap(array, i+1, pivot)
+    return i+1
+
+
+def qs(array, f, l):
+    if f<l:
+        p = partition(array, f, l)
+        histmatrix = [[0 for i in range(width)]for j in range(height)]
+        time.sleep(0.3)
+        os.system('clear')
+        createAndDisplay(array, histmatrix, width, height)
+        a = input('Press Enter')
+        qs(array, f, p-1)
+        qs(array, p+1, l)
+
+
+def quickSort(array):
+    qs(array, 0, len(array)-1)
+    return array
+
+
+
+def createAndDisplay(observations, histmatrix, numberofobservations, rangeofvalues):
+    for i in range(numberofobservations):
+        for j in range(rangeofvalues):
+            newi = rangeofvalues-1-observations[i]
+            if j<=newi:
+                histmatrix[j][i]=0
+            else:
+                histmatrix[j][i]=1
+
+    # print (observations)
+    for i in histmatrix:
+        for j in i:
+            if j==1:
+                print ("| ",end="")
+            else:
+                print ("  ",end="")
+        print()
+
+
+
+
+height = 30
+width = 30
+di = {}
+array = []
+i = 0
+
+while i<width:
+    num = random.randint(1,height)
+    if num in di:
+        continue
+    else:
+        i+=1
+        di[num] = 1
+        array.append(num)
+
+array = quickSort(array)
+#print(array)
