811fb0a825deb7721864314013efe50df618abec
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-08-18 22:02:45 +0530

    completed easyhttp and added other content in myjs

---
+++ myjs/easyhttp/app3.js
@@ -0,0 +1,30 @@
+const http = new EasyHTTP();
+
+
+// http.get('https://jsonplaceholder.typicode.com/users')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+
+const data = {
+    name:'Jack',
+    username: 'jackfrost',
+    email: '123@gmail.com'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+// http.put('https://jsonplaceholder.typicode.com/users/8', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+
+
+http.delete('https://jsonplaceholder.typicode.com/users/9')
+.then(data => {
+    console.log(data);
+})
+.catch(err => console.log(err));
+
